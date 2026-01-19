07c077d10ae44780e15aab76b01d981153b7ee49
noah-truong <noahtruong123@gmail.com>
2026-01-19 11:53:41 -0800

    fourier

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -27,6 +27,12 @@ function Projects() {
       title: '12o1b',
       image: '/assets/12o1b.png',
       link: 'https://12o1b.vercel.app/'
+    },
+    {
+      id: 'project4',
+      title: 'Fourier Transform Visualizer',
+      image: '/assets/fourier.png',
+      link: 'https://fourier-app.vercel.app/'
     }
   ];
 
